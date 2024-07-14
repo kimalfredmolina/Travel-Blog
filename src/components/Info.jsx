99c58d5906d14c4f5ae61ec529a506cc257c788d
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-07-14 21:46:03 +0800

    Added  Column Boxes

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,6 +3,11 @@ import luz2 from "../assets/luz2.jpeg";
 import luz3 from "../assets/luz3.jpg";
 import luz4 from "../assets/luz4.jpg";
 
+import vis1 from "../assets/vis1.jpg";
+import vis2 from "../assets/vis2.jpg";
+import vis3 from "../assets/vis3.webp";
+import vis4 from "../assets/vis4.webp";
+
 const Info = () => {
   return (
     <div className="container mx-auto p-4">
@@ -45,27 +50,27 @@ const Info = () => {
         <h2 className="text-2xl font-bold mb-4 text-black">Visayas Tourist Spot</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           <div className="card bg-white shadow-md rounded-lg p-4 relative">
-            <img src="../assets/luz5.jpg" alt="Tvis1" className="rounded-lg"/>
-            <div className="absolute bottom-4 left-4 text-white">
-              <h3 className="text-xl font-semibold">Tanzania</h3>
+            <img src={vis1} alt="Tvis1" className="rounded-lg"/>
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white w-full text-center">
+              <h3 className="text-xl font-semibold">Cebu</h3>
             </div>
           </div>
           <div className="card bg-white shadow-md rounded-lg p-4 relative">
-            <img src="../assets/luz6.jpg" alt="vis2" className="rounded-lg"/>
-            <div className="absolute bottom-4 left-4 text-white">
-              <h3 className="text-xl font-semibold">Vietnam</h3>
+            <img src={vis2} alt="vis2" className="rounded-lg"/>
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white w-full text-center">
+              <h3 className="text-xl font-semibold">Boracay, Aklan</h3>
             </div>
           </div>
           <div className="card bg-white shadow-md rounded-lg p-4 relative">
-            <img src="../assets/luz7.jpg" alt="vis3" className="rounded-lg"/>
-            <div className="absolute bottom-4 left-4 text-white">
-              <h3 className="text-xl font-semibold">Nepal</h3>
+            <img src={vis3} alt="vis3" className="rounded-lg"/>
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white w-full text-center">
+              <h3 className="text-xl font-semibold">Bohol</h3>
             </div>
           </div>
           <div className="card bg-white shadow-md rounded-lg p-4 relative">
-            <img src="../assets/luz8.jpg" alt="vis4" className="rounded-lg"/>
-            <div className="absolute bottom-4 left-4 text-white">
-              <h3 className="text-xl font-semibold">Nicaragua</h3>
+            <img src={vis4} alt="vis4" className="rounded-lg"/>
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white w-full text-center">
+              <h3 className="text-xl font-semibold">Guimaras</h3>
             </div>
           </div>
         </div>
@@ -76,25 +81,25 @@ const Info = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           <div className="card bg-white shadow-md rounded-lg p-4 relative">
             <img src="../assets/luz5.jpg" alt="min1" className="rounded-lg"/>
-            <div className="absolute bottom-4 left-4 text-white">
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white w-full text-center">
               <h3 className="text-xl font-semibold">Tanzania</h3>
             </div>
           </div>
           <div className="card bg-white shadow-md rounded-lg p-4 relative">
             <img src="../assets/luz6.jpg" alt="min2" className="rounded-lg"/>
-            <div className="absolute bottom-4 left-4 text-white">
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white w-full text-center">
               <h3 className="text-xl font-semibold">Vietnam</h3>
             </div>
           </div>
           <div className="card bg-white shadow-md rounded-lg p-4 relative">
             <img src="../assets/luz7.jpg" alt="min3" className="rounded-lg"/>
-            <div className="absolute bottom-4 left-4 text-white">
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white w-full text-center">
               <h3 className="text-xl font-semibold">Nepal</h3>
             </div>
           </div>
           <div className="card bg-white shadow-md rounded-lg p-4 relative">
             <img src="../assets/luz8.jpg" alt="min4" className="rounded-lg"/>
-            <div className="absolute bottom-4 left-4 text-white">
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white w-full text-center">
               <h3 className="text-xl font-semibold">Nicaragua</h3>
             </div>
           </div>
