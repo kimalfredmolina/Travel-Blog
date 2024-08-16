dff21fc5b349b3f4af4ce2f1f71d0b660d71e8fa
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-08-16 19:38:38 +0800

    Added Responsoe navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import logo from "../assets/ph.png";
-import { FaUser } from "react-icons/fa";
+import { FaUser, FaBars, FaTimes } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="fixed top-0 left-0 w-full text-black z-50 font-poppins bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -12,13 +19,18 @@ const Navbar = () => {
               <img src={logo} alt="Logo" className="h-10" />
             </a>
           </div>
-          <div className="flex-1 flex justify-center items-center space-x-10 text-lg">
-            <a href="#home" className="hover:text-orange-600 font-bold">Home</a>
-            <a href="#about" className="hover:text-orange-600 font-bold">About</a>
-            <a href="#info" className="hover:text-orange-600 font-bold">Tourist Spot</a>
-            <a href="#services" className="hover:text-orange-600 font-bold">Services</a>
-            <a href="#pricing" className="hover:text-orange-600 font-bold">Pricing</a>
-            <a href="#contact" className="hover:text-orange-600 font-bold">Contact</a>
+          <div className="lg:hidden">
+            <button onClick={toggleMenu} className="text-black focus:outline-none">
+              {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </button>
+          </div>
+          <div className={`lg:flex flex-1 justify-center items-center lg:space-x-10 text-lg ${isOpen ? 'absolute top-16 left-0 w-full bg-white p-4 space-y-4' : 'hidden'} lg:block`}>
+            <a href="#home" className="block lg:inline-block text-center hover:text-orange-600 font-bold">Home</a>
+            <a href="#about" className="block lg:inline-block text-center hover:text-orange-600 font-bold">About</a>
+            <a href="#info" className="block lg:inline-block text-center hover:text-orange-600 font-bold">Tourist Spot</a>
+            <a href="#services" className="block lg:inline-block text-center hover:text-orange-600 font-bold">Services</a>
+            <a href="#pricing" className="block lg:inline-block text-center hover:text-orange-600 font-bold">Pricing</a>
+            <a href="#contact" className="block lg:inline-block text-center hover:text-orange-600 font-bold">Contact</a>
           </div>
           <div className="flex-shrink-0 hover:text-orange-600 transition-colors duration-300">
             <Link to="/login">
